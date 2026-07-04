766f97f4a5aef9e9045e1bf14b8de740eb8fb878
hafid <hafidkarkouch5@gmail.com>
2026-07-04 19:07:42 +0100

    Add detailed comments to ex01.c for clarity on program functionality

--- exercice/ex01.c
+++ exercice/ex01.c
@@ -1,5 +1,14 @@
 #include <unistd.h>
 
+/*
+** Exercise: ex00
+** Name: Hello Hafid
+** Description:
+**   This program prints two messages to the standard output
+**   using the write system call.
+**   It demonstrates basic usage of write() in C.
+*/
+
 int main(void)
 {
     write(1, "Hello Hafid\n", 12);
