8488ca50fadea61c294197cfd3f7c31cc1de765b
hafid <hafidkarkouch5@gmail.com>
2026-07-04 19:11:05 +0100

    Add function description and comments to ex03.c for clarity

--- exercice/ex03.c
+++ exercice/ex03.c
@@ -1,14 +1,24 @@
-
 #include <unistd.h>
 
-int main (void){
-    int i=9;
+/*
+** Exercise: ex03
+** Name: ft_print_reverse_numbers
+** Description:
+**   This program prints the digits from 9 to 0
+**   in reverse order using a while loop and write().
+**   It uses ASCII conversion ('0') to print characters.
+*/
+
+int main (void)
+{
+    int i = 9;
     char a;
-    while (i>=0)
+
+    while (i >= 0)
     {
-        a=i+'0';
-        write(1,&a,1);
+        a = i + '0';
+        write(1, &a, 1);
         i--;
     }
- return 0;
+    return 0;
 }
