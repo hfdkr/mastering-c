c4b685e15cb8a6a604a728d6216e4bff854978fe
hafid <hafidkarkouch5@gmail.com>
2026-07-04 19:08:10 +0100

    Add function description and comments to ex02.c for clarity

--- exercice/ex02.c
+++ exercice/ex02.c
@@ -1,10 +1,23 @@
 #include <unistd.h>
-int main (void) {
-    int i=0;
+
+/*
+** Exercise: ex02
+** Name: ft_print_numbers
+** Description:
+**   This program prints the digits from 0 to 9
+**   using a while loop and the write system call.
+**   It converts integers to characters using ASCII ('0').
+*/
+
+int main(void)
+{
+    int i = 0;
     char c;
-    while (i<10) {
-        c=i+'0';
-        write(1,&c,1);
+
+    while (i < 10)
+    {
+        c = i + '0';
+        write(1, &c, 1);
         i++;
     }
     return 0;
