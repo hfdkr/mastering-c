186fa8f61f80446504d89329eab5d631b811337f
hafid <hafidkarkouch5@gmail.com>
2026-07-04 19:26:47 +0100

    Enhance comments for clarity and structure in ex06.c

--- C00/ex06.c
+++ C00/ex06.c
@@ -1,12 +1,25 @@
 #include <unistd.h>
-int main (void){
-    int i=0;
+
+/*
+** Exercise: practice
+** Description:
+**   This program prints digits from 0 to 9,
+**   each number followed by a newline.
+**   It demonstrates loops, ASCII conversion, and write().
+*/
+
+int main(void)
+{
+    int i;
     char c;
 
-    while(i<10){
-        c=i+'0';
-        write(1,&c,1);
-        write(1,"\n",1);
+    i = 0;
+    while (i < 10)
+    {
+        c = i + '0';
+        write(1, &c, 1);
+        write(1, "\n", 1);
         i++;
     }
+    return 0;
 }
